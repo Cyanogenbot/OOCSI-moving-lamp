d9748f1412dc559b90c001cccdfacb834be65b9d
Diede van Marle <diedeasus@gmail.com>
2022-02-03 14:03:11 +0100

    Add files via upload

---
+++ oocsi%20light%20example_copy_20211223172742.ino
@@ -0,0 +1,164 @@
+#include <ArduinoHttpClient.h>
+#include <Adafruit_NeoPixel.h>
+#include <SPI.h>
+#include <OOCSI.h>
+#include <OOCSIDevice.h>
+#include <SpeedyStepper.h>
+
+#define LED_PINR     15
+#define LED_PINL     25
+#define LED_COUNTL  78
+#define LED_COUNTR  78
+
+Adafruit_NeoPixel stripL(LED_COUNTL, LED_PINL, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel stripR(LED_COUNTR, LED_PINR, NEO_GRBW + NEO_KHZ800);
+SpeedyStepper stepper;
+OOCSI oocsi = OOCSI();
+
+const int EN_PIN = 7;
+const int MOTOR_STEP_PIN = 11;
+const int MOTOR_DIRECTION_PIN = 12;
+
+int lastColour[] = {255, 255, 255, 255};
+int brightnessNormal;
+int brightnessNormalOld;
+int brightnessWhite;
+int brightnessWhiteOld;
+// put your setup code here, to run once:
+void setup() {
+  pinMode(EN_PIN, OUTPUT);
+
+  digitalWrite(EN_PIN, HIGH); 
+  Serial.begin(9600);
+  stripL.begin();
+  stripR.begin();
+
+
+  // Set default colour
+  stepper.connectToPins(MOTOR_STEP_PIN, MOTOR_DIRECTION_PIN);
+  // Set up oocsi server
+  oocsi.connect("NAME", "oocsi_server_ip", "SSID", "WIFIpassword", processOOCSI);
+  // Create and send heyoocsi device
+  OOCSIDevice prototype = oocsi.heyOOCSI("device_name");
+  prototype.addLight(
+    "MyFirstLight",
+    "MyFirstLightChannel",
+    "RGBW",
+    LIGHT_SPECTRUM_RGB_CCT_WHITE,
+    "True",
+    100,
+    "desklamp"
+  );
+  prototype.sayHi();
+  // listen to oocsi channel messages
+  oocsi.subscribe("MyFirstLightChannel");
+}
+// put your main code here, to run repeatedly:
+void loop() {
+  oocsi.check();
+  delay(500);
+}
+void processOOCSI() {
+  Serial.println("open");
+
+  Serial.println("stepperset");
+
+  // Toggle leds on or off, and use last used colour
+  // if (oocsi.has("state")){
+  //   String State = oocsi.getString("state","False");
+  //   if (State == "True"){
+  //     stripL.setBrightness(100);
+  //     // stepper.moveRelativeInSteps(-20000);
+  //     stripR.setBrightness(100);
+  //     for (int i = 0; i < LED_COUNTL; i++){
+  //       stripL.setPixelColor(i, stripL.Color(lastColour[0], lastColour[1], lastColour[2], lastColour[3]));
+  //       Serial.println("leftcol");
+  //       stripR.setPixelColor(i, stripR.Color(lastColour[0], lastColour[1], lastColour[2], lastColour[3]));
+  //       Serial.println("rightcol");
+
+  //       digitalWrite(EN_PIN, LOW); 
+
+  //       // stepper.moveToPositionInRevolutions(-3.5);
+
+  //       digitalWrite(EN_PIN, HIGH); 
+  //       }
+  //     }
+  //   else{
+  //     stripL.setBrightness(0);
+  //     stripR.setBrightness(0);
+  //     // stepper.moveRelativeInSteps(20000);
+  //     }
+  //   }
+
+    
+  
+
+  // Change colour on demand
+  if (oocsi.has("colorrgbw")){
+    int standardarray[] = {255, 255, 255, 255};
+    // int results[] = {0, 0, 0, 0};
+    oocsi.getIntArray("colorrgbw", standardarray, lastColour, 4);
+    // lastColour = results[];
+    for (int colors = 0; colors < LED_COUNTL; colors++){
+      stripL.setPixelColor(colors, stripL.Color(lastColour[0], lastColour[1], lastColour[2], lastColour[3]));
+      Serial.println("leftcol");
+      stripR.setPixelColor(colors, stripR.Color(lastColour[0], lastColour[1], lastColour[2], lastColour[3]));
+      Serial.println("rightcol");
+      }
+  }
+
+  // if (oocsi.has("colortemp in rgb)){
+  //   int standardarray[] = {255, 255, 255, 255};
+  //   int results[] = {0, 0, 0,0};
+  //   oocsi.getIntArray("colorrgbw", standardarray, results, 4);
+  //   lastColour = results
+  //   for (int i=0; i< LED_COUNTL;i++){
+  //     stripL.setPixelColor(i, stripL.Color(results[0], results[1], results[2], results[3]));
+  //     Serial.println("leftcol");
+  //     stripR.setPixelColor(i, stripR.Color(results[0], results[1], results[2], results[3]));
+  //     Serial.println("rightcol");}
+  // }
+
+
+  // Change brightness on demand
+  if (oocsi.has("brightness")){
+    Serial.println("brightness");
+    brightnessNormal = oocsi.getInt("brightness", 0);
+    
+    stripL.setBrightness(brightnessNormal);
+    Serial.println("leftbright");
+    stripR.setBrightness(brightnessNormal);
+    Serial.println("rightbright");
+    stripL.show();
+    stripR.show();
+    digitalWrite(EN_PIN, LOW);
+    // Serial.println("pin low");
+    stepper.setStepsPerMillimeter(1600/8);
+    stepper.setSpeedInMillimetersPerSecond(40);
+    stepper.setAccelerationInMillimetersPerSecondPerSecond(30);
+    int distance = (260 / 255 * brightnessNormal);
+    stepper.moveToPositionInMillimeters(distance);
+    Serial.println("turn");
+    digitalWrite(EN_PIN, HIGH); 
+    brightnessNormalOld = brightnessNormal;
+    // Serial.println("pin high");
+  }
+
+  // Change to white mode brightness on demand
+  if (oocsi.has("brightnessWhite")){
+    int brightnessWhite = oocsi.getInt("brightnessWhite", 0);
+    stripL.setBrightness(brightnessWhite);
+    Serial.println("leftbrightwhite");
+    stripR.setBrightness(brightnessWhite);
+    Serial.println("rightbrightwhite");
+    for (int i2 = 0; i2 < LED_COUNTL; i2 ++){
+      stripL.setPixelColor(i2, stripL.Color(0,0,0, 255));
+      Serial.println("leftwhite");
+      stripR.setPixelColor(i2, stripR.Color(0,0,0, 255));
+      Serial.println("rightwhite");
+      }
+  }
+  stripL.show();
+  stripR.show();
+  
+}
