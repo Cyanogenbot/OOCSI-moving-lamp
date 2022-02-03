062b2c24db4de543d9a08b6e8c2db5d7a03944f3
Diede van Marle <diedeasus@gmail.com>
2022-02-03 14:18:16 +0100

    Added hints

--- lampcode.ino
+++ lampcode.ino
@@ -10,36 +10,52 @@
 #define LED_COUNTL  78
 #define LED_COUNTR  78
 
+// define ledstrips
 Adafruit_NeoPixel stripL(LED_COUNTL, LED_PINL, NEO_GRBW + NEO_KHZ800);
 Adafruit_NeoPixel stripR(LED_COUNTR, LED_PINR, NEO_GRBW + NEO_KHZ800);
-SpeedyStepper stepper;
-OOCSI oocsi = OOCSI();
-
-const int EN_PIN = 7;
-const int MOTOR_STEP_PIN = 11;
-const int MOTOR_DIRECTION_PIN = 12;
 
 int lastColour[] = {255, 255, 255, 255};
 int brightnessNormal;
 int brightnessNormalOld;
 int brightnessWhite;
 int brightnessWhiteOld;
+
+//define stepper
+SpeedyStepper stepper;
+const int EN_PIN = 7;
+const int MOTOR_STEP_PIN = 11;
+const int MOTOR_DIRECTION_PIN = 12;
+
+//define oocsi
+OOCSI oocsi = OOCSI();
+
+
 // put your setup code here, to run once:
 void setup() {
-  pinMode(EN_PIN, OUTPUT);
 
+  // Disable steppers
+  pinMode(EN_PIN, OUTPUT);
   digitalWrite(EN_PIN, HIGH); 
+
+  // Start strips and serial
   Serial.begin(9600);
   stripL.begin();
   stripR.begin();
 
 
-  // Set default colour
+  // Setup stepper
   stepper.connectToPins(MOTOR_STEP_PIN, MOTOR_DIRECTION_PIN);
+
   // Set up oocsi server
   oocsi.connect("NAME", "oocsi_server_ip", "SSID", "WIFIpassword", processOOCSI);
+
   // Create and send heyoocsi device
   OOCSIDevice prototype = oocsi.heyOOCSI("device_name");
+
+  // Add signature to HA
+  prototype.property("creator", "Diede!");
+
+  // Add light
   prototype.addLight(
     "MyFirstLight",
     "MyFirstLightChannel",
@@ -49,8 +65,11 @@ void setup() {
     100,
     "desklamp"
   );
+
+  // Send heyoocsi
   prototype.sayHi();
-  // listen to oocsi channel messages
+
+  // Listen to oocsi channel messages
   oocsi.subscribe("MyFirstLightChannel");
 }
 // put your main code here, to run repeatedly:
@@ -63,6 +82,8 @@ void processOOCSI() {
 
   Serial.println("stepperset");
 
+
+// still broken :(
   // Toggle leds on or off, and use last used colour
   // if (oocsi.has("state")){
   //   String State = oocsi.getString("state","False");
@@ -99,6 +120,8 @@ void processOOCSI() {
     // int results[] = {0, 0, 0, 0};
     oocsi.getIntArray("colorrgbw", standardarray, lastColour, 4);
     // lastColour = results[];
+
+    // Change strip colour on command
     for (int colors = 0; colors < LED_COUNTL; colors++){
       stripL.setPixelColor(colors, stripL.Color(lastColour[0], lastColour[1], lastColour[2], lastColour[3]));
       Serial.println("leftcol");
@@ -125,6 +148,7 @@ void processOOCSI() {
     Serial.println("brightness");
     brightnessNormal = oocsi.getInt("brightness", 0);
     
+    // Set strip brightness
     stripL.setBrightness(brightnessNormal);
     Serial.println("leftbright");
     stripR.setBrightness(brightnessNormal);
@@ -133,6 +157,8 @@ void processOOCSI() {
     stripR.show();
     digitalWrite(EN_PIN, LOW);
     // Serial.println("pin low");
+
+    // Move steppers according to brightness
     stepper.setStepsPerMillimeter(1600/8);
     stepper.setSpeedInMillimetersPerSecond(40);
     stepper.setAccelerationInMillimetersPerSecondPerSecond(30);
@@ -146,11 +172,15 @@ void processOOCSI() {
 
   // Change to white mode brightness on demand
   if (oocsi.has("brightnessWhite")){
+    
+    // Set white mode brightness
     int brightnessWhite = oocsi.getInt("brightnessWhite", 0);
     stripL.setBrightness(brightnessWhite);
     Serial.println("leftbrightwhite");
     stripR.setBrightness(brightnessWhite);
     Serial.println("rightbrightwhite");
+
+    // Change colors to white
     for (int i2 = 0; i2 < LED_COUNTL; i2 ++){
       stripL.setPixelColor(i2, stripL.Color(0,0,0, 255));
       Serial.println("leftwhite");
